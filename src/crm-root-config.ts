1e68f40bdfa4f0dcf34cc0b6b3cb406a3e65055b
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-09-06 15:09:12 -0300

    chore: config port

--- src/crm-root-config.ts
+++ src/crm-root-config.ts
@@ -1,19 +1,40 @@
 import { registerApplication, start, type LifeCycles } from 'single-spa'
 
 registerApplication({
-	name: '@single-spa/welcome',
-	app: () =>
-		System.import<LifeCycles>(
-			'https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js',
-		),
+	name: '@single-spa/dashboard',
+	app: () => System.import<LifeCycles>('@crm/dashboard'),
+	activeWhen: location => location.pathname === '/',
+})
+
+registerApplication({
+	name: '@single-spa/gallery',
+	app: () => System.import<LifeCycles>('@crm/gallery'),
+	activeWhen: location => location.pathname === '/gallery',
+})
+
+registerApplication({
+	name: '@single-spa/blog',
+	app: () => System.import<LifeCycles>('@crm/blog'),
+	activeWhen: ['/blog'],
+})
+
+registerApplication({
+	name: '@single-spa/aside',
+	app: () => System.import<LifeCycles>('@crm/aside'),
 	activeWhen: ['/'],
 })
 
-// registerApplication({
-//   name: "@crm/navbar",
-//   app: () => System.import("@crm/navbar"),
-//   activeWhen: ["/"]
-// });
+registerApplication({
+	name: '@single-spa/header',
+	app: () => System.import<LifeCycles>('@crm/header'),
+	activeWhen: ['/'],
+})
+
+registerApplication({
+	name: '@single-spa/footer',
+	app: () => System.import<LifeCycles>('@crm/footer'),
+	activeWhen: ['/'],
+})
 
 start({
 	urlRerouteOnly: true,
